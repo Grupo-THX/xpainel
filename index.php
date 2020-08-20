7ac2653f2fb173eeb130ec18a4303c2153885368
Jardel Jacomini <deljdl@gmail.com>
2020-08-20 11:11:45 -0300

    a

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use ourcodeworld\HelloComposer\Hello;
+
+$instance = new Hello();
+
+echo $instance->say("Hello World");
+
+echo 'ok';
---
+++ src/X.php
@@ -0,0 +1,10 @@
+<?php
+namespace ourcodeworld\HelloComposer;
+
+class Hello
+{
+    public function say($toSay = "Nothing given")
+    {
+        return $toSay;
+    }
+}
